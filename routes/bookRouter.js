e348560529034511e15c1f2f29c8c683ba3b07b4
sam-knowles <thesamknowles@gmail.com>
2022-09-20 12:24:35 -0600

    got all the routes working through postman

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,22 +1,61 @@
 const express = require('express')
 const bookRouter = express.Router()
+const { v4: uuid } = require('uuid');
 
 const books = [
-    { title: 'Homelander', genre: "action" },
-    { title: 'Basketball', genre: "sports" },
-    { title: 'Kissing', genre: "action" },
-    { title: 'Tennis', genre: "sports" }
+    { title: 'Homelander', genre: "action", _id: uuid() },
+    { title: 'Basketball', genre: "sports", _id: uuid() },
+    { title: 'Kissing', genre: "action", _id: uuid() },
+    { title: 'Tennis', genre: "sports", _id: uuid() }
 ]
 
 bookRouter.get("/", (req, res) => {
     res.send(books)
 })
 
+bookRouter.get("/:bookId", (req, res) => {
+    const bookId = req.params.bookId
+    const foundBook = books.find(book => book._id === bookId)
+    res.send(foundBook)
+})
+
+bookRouter.get("/search/genre", (req, res) => {
+    const genre = req.query.genre
+    const filteredBooks = books.filter(book => book.genre === genre)
+    res.send(filteredBooks)
+})
+
 bookRouter.post("/", (req, res) => {
     const newBook = req.body
+    newBook._id = uuid()
     books.push(newBook)
     res.send(`Successfully added ${newBook.title} to the database`)
 })
+
+bookRouter.delete("/:bookId", (req, res) => {
+    const bookId = req.params.bookId
+    const bookIndex = books.findIndex(book => book._id === bookId)
+    books.splice(bookIndex, 1)
+    res.send("successfully delete book")
+})
+
+bookRouter.put('/:bookId', (req, res) => {
+    const bookId = req.params.bookId
+    const updatedObject = req.body
+    const bookIndex = books.findIndex(book => book._id === bookId)
+    const updatedBook = Object.assign(books[bookIndex], updatedObject)
+    res.send(updatedBook)
+})
+
+// bookRouter.route("/")
+//     .get((req, res) => {
+//         res.send(books)
+//     })
+//     .post((req, res) => {
+//         const newBook = req.body
+//         books.push(newBook)
+//         res.send(`Successfully added ${newBook.title} to the database!`)
+//     })
 
 
 
