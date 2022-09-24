bd28a2562718bb2b584e2bb395c8e6d35b237c75
sam-knowles <thesamknowles@gmail.com>
2022-09-23 18:06:58 -0600

    added a user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    memberSince: {
+        type: Date,
+        default: Date.now
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = moongoose.model("user, userSchema")
