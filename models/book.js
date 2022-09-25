2e5987cb55ce354816fe84de76b25f85949ef5e3
sam-knowles <thesamknowles@gmail.com>
2022-09-25 14:17:17 -0600

    ran into cannot read properties of undefined _id

--- models/book.js
+++ models/book.js
@@ -9,6 +9,11 @@ const bookSchema = new Schema({
     genre: {
         type: String,
         required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     }
 })
 
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const {expressjwt} = require('express-jwt')
+
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -10,10 +12,14 @@ app.use(morgan('dev'))
 mongoose.connect("mongodb://localhost:27017/bookdb", () => console.log('connected to database'))
 
 app.use("/auth", require("./routes/authRouter.js"))
-app.use("/books", require("./routes/bookRouter.js"))
+app.use("/api", expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
+app.use("/api/books", require("./routes/bookRouter.js"))
 
 app.use((err, req, res, next) => {
     console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
     return res.send({errMsg: err.message})
 })
 
