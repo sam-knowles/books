c83370c08ae8989b9fa36fdcf28796eec45c4374
sam-knowles <thesamknowles@gmail.com>
2022-09-26 11:14:43 -0600

    added an authjs file that needs to be moved to appjs

---
+++ client/src/components/Auth.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route} from 'react-router-dom'
+import Navbar from './components/Navbar.js'
+import Auth from './components/Auth.js'
+import Profile from './components/Profile.js'
+import Public from './components/Public.js'
+import App from '../App.js'
+
+export default function App() {
+    return (
+        <div> 
+        <Navbar />
+        <Switch>
+            <Route
+                exact path='/'
+                render={() => <Auth />}
+            />
+            <Route
+                exact path='/profile'
+                render={() => <Profile />}
+            />
+            <Route
+                exact path='/public'
+                render={() => <Public />}
+            />
+        </Switch>
+        </div>
+    )
+}
