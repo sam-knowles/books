e3d82778c56fb462573d33b38153bd243e294738
sam-knowles <thesamknowles@gmail.com>
2022-09-29 15:07:59 -0600

    added navbar folder and guts

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar(){
+    return (
+        <div>
+            <link to="/profile">Profile</link>
+            <link t0="/public"> Public</link>
+        </div>
+    )
+}
